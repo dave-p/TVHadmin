4f6360ba3851e85db5a2453e5461f159a07b9f79
dave-p <gh@pickles.me.uk>
2017-03-16 11:22:32 +0000

    Fix exit from Configure screen.

--- configure.php
+++ configure.php
@@ -2,5 +2,5 @@
 	include_once "include.php";
 	$enc = json_encode($_POST);
 	file_put_contents($config_file, $enc);
-	echo "<script>top.window.location='fav.html'</script>"; 
+	header('Location: fav.php'); 
 ?>
