22f00d1590668573c5ee00a7e9dfd4866e7b71b9
dave-p <gh@pickles.me.uk>
2017-03-21 10:53:02 +0000

    Use summary, not subtitle, is search results.

--- search.php
+++ search.php
@@ -16,8 +16,6 @@
 		echo "<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\" id=\"heading\">";
 		echo "<tr><td class=\"col_title\"><h1>Matches for: <i>$find</i></h1></td></tr></table>";
 		echo "<table border=\"0\" cellspacing=\"0\" cellpadding=\"0\" width=\"100%\" class=\"list hilight\" id=\"content\">";
-		$last_prog_date = 'unknown';
-#		$query = "SELECT date_format(prog.time,'%a %e %M'), hour(prog.time),minute(prog.time),prog.title,prog.descr,prog.critem,prog.crseries,chan.name FROM prog,chan";
 		$i = 0;
 		$last_prog_date = " ";
 		$results = search_epg("", $find);
@@ -36,7 +34,7 @@
 			}
 			echo "<td class=\"col_duration\"><span class=\"time_duration\"><span class=\"time_start\">";
 			printf("%s</span></span></td><td class=\"col_channel\"><div class=\"channel_name\">%s</div></td>", $t,$r["channelName"]);
-			printf("<td class=\"col_center\"><div class=\"epg_title\">%s</div><div class=\"epg_subtitle\">%s</div></td>", $r["title"],$r["subtitle"]);
+			printf("<td class=\"col_center\"><div class=\"epg_title\">%s</div><div class=\"epg_subtitle\">%s</div></td>", $r["title"],$r["summary"]);
 			printf("</tr>\n");
 			$i++;
 		}
