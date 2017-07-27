0d03e99af3b34f664ba9cadadbb04b788b00199e
dave-p <git@pickles.me.uk>
2017-07-27 08:20:53 +0100

    Correct URL.

--- delete-series.php
+++ delete-series.php
@@ -1,7 +1,7 @@
 <?php
 	include_once "./include.php";
 	$uuid = $_GET["uuid"];
-	$url = "$urlp/api/dvr/idnode/delete?uuid=$uuid";
+	$url = "$urlp/api/idnode/delete?uuid=$uuid";
 	curl_file_get_contents($url);
 	header('Location: links.php');
 ?>
