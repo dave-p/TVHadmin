57aa727616e0a5c374d58f4466cdae946f68cf30
dave-p <git@pickles.me.uk>
2019-05-23 12:27:25 +0100

    Links: count recordings for each link, improve mobile layout.

--- links.php
+++ links.php
@@ -19,16 +19,18 @@
       <div id='content'>
        <table class='list'>
 	<tr class='heading'>
-	  <td class='col_start'><h2>Timers</h2></td>
+	  <td class='col_value'><h2>Timers</h2></td>
+	  <td class='col_value'><h2>Recs</h2></td>
 	  <td class='col_channel'><h2>Channel</h2></td>
-	  <td class='col_channel'><h2>Link</h2></td>
-	  <td class='col_name'><h2>Name</h2></td> 
+	  <td class='col_name'><h2>Name</h2></td>
+	  <td class='wideonly col_channel'><h2>Link</h2></td>
 	  <td class='col_delete'></td>
 	</tr>
   ";
         $links = get_links();
 	$channels = get_channels();
 	$timers = get_timers();
+	$recordings = get_recordings(-1);
 	$i = 0;
 	foreach($links as $l) {
 	    if ($i % 2) {
@@ -48,13 +50,19 @@
 	    foreach($timers as $t) {
 		if ($t["autorec"] === $l["uuid"]) $n++;
 	    }
+	    $recs = 0;
+	    foreach($recordings as $r) {
+		if ($r["sched_status"] == "scheduled") continue;
+		if ((isset($r['autorec'])) && ($l['uuid'] === $r['autorec'])) $recs++;
+	    }
 	    $l['title'] = stripslashes($l['title']);
 	    $crid = substr(strstr($l['serieslink'], '//'), 2);
 	    echo "
-	<td class='col_start'>$n</td>
+	<td class='col_value'>$n</td>
+	<td class='col_value'>$recs</td>
 	<td class='col_channel'>$channelname</td>
-	<td class='col_channel'>$crid</td>
 	<td class='col_name'>{$l['title']}</td>
+	<td class='wideonly col_channel'>$crid</td>
 	<td class='col_delete'><a href='links.php?uuid={$l['uuid']}'><img src='images\delete.png'></a></td>
       </tr>\n";
 	    $i++;
