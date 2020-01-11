1cd071b4d3741ac657b1a9dc0defb26eddea9759
dave-p <git@pickles.me.uk>
2020-01-11 11:13:34 +0000

    Partially revert 9df7224 - replace auth for streaming.

--- config.php
+++ config.php
@@ -243,15 +243,24 @@
 		</td>
 	    </tr>
 	    <tr class='row_even'>
-		<td class='col_label'><h5>Detect timer clashes (single tuner only):</h5></td>
-		<td class='col_value'><input type='checkbox' name='CLASHDET'";
-	  if (isset($settings['CLASHDET'])) {
-	    echo " checked";
-	  }
-	  echo ">
-	      </td>
+		<td class='col_label'><h5>Send user/pass when Viewing:</h5></td>
+		<td class='col_value'><input type='checkbox' name='NOANON'";
+        if (isset($settings['NOANON'])) {
+          echo " checked";
+        }
+        echo ">
+		</td>
 	    </tr>
 	    <tr class='row_odd'>
+		<td class='col_label'><h5>Detect timer clashes (single tuner only):</h5></td>
+		<td class='col_value'><input type='checkbox' name='CLASHDET'";
+	if (isset($settings['CLASHDET'])) {
+	  echo " checked";
+	}
+	echo ">
+	      </td>
+	    </tr>
+	    <tr class='row_even'>
 		<td class='col_label'><h5>Timeline length:</h5></td>
 		<td class='col_value'>
 		    <select name='TIMESPAN'>";
--- now.php
+++ now.php
@@ -20,6 +20,8 @@
 	$wday = date('l, j M Y', time());
 	$time = date('H:i', time());
 	$media = array();
+	if (array_key_exists('NOANON', $settings)) $view_url = "http://$user:$pass@$ip";
+	else $view_url = $urlp;
 	if (isset($settings["CSORT"]) && ($settings["CSORT"] == 1)) $lcn = 1;
 	else $lcn = 0;
 
@@ -125,7 +127,7 @@ good:
 	echo "
       </td>
       <td class='col_stream'>
-	<a href='$urlp/play/stream/channel/{$c['uuid']}?title={$c['name']}'><img src='images\play.png' title='Play'></a>
+	<a href='$view_url/play/stream/channel/{$c['uuid']}?title={$c['name']}'><img src='images\play.png' title='Play'></a>
       </td>
      </tr>";
 		$i++;
