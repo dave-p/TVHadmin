dave-p/TVHadmin

commit 970d2dc1163f91f3d9c94212bcc3486b0783d520

Use CSS to select background of alternate rows.

timers.php
@@ -39,7 +39,6 @@
 	</tr>
   ";
 	$autorecs = get_autorecs();
-	$i = 0;
 	$channels = array();
 	$clashes = array();
 	foreach($timers as $t) {
@@ -48,12 +47,7 @@
 	    $stop = strftime("%H:%M", $t["stop_real"]);
 	    $date = strftime("%a %e/%m", $t["start_real"]);
 	    $subtitle = $t["disp_extratext"];
-	    if ($i % 2) {
-		echo "<tr class='row_odd' title=\"$subtitle\">";
-	    }
-	    else {
-		echo "<tr class='row_even' title=\"$subtitle\">";
-	    }
+	    echo "<tr class='row_alt' title='$subtitle'>";
 	    if ($t["start_real"] < $now) {
 		echo "<td class='col_info'><img src='images/rec.png'></td>";
 	    }
@@ -92,7 +86,6 @@
 	<a href='timers.php?uuid={$t['uuid']}'><img src='images\delete.png' title='Delete Timer'></a>
       </td>
     </tr>\n";
-	    $i++;
 	}
 	echo "</table>\n";
 	foreach ($clashes as $c) {

config.php
@@ -63,26 +63,26 @@
 	    <tr class="heading">
 		<td colspan="2"><h2>TVHeadend Server</h2></td>
 	    </tr>
-	    <tr class="row_odd" title="Name of a TVheadend user with admin privilege">
+	    <tr class="row_alt" title="Name of a TVheadend user with admin privilege">
 		<td class="col_label"><h5>Username:</h5></td>
 <?php
 		echo "<td class='col_value'><input type='text' name='USER' value='$user' size='16'>";
 ?>
 		</td>
 	    </tr>
-	    <tr class="row_even" title="Password of the TVheadend user">
+	    <tr class="row_alt" title="Password of the TVheadend user">
 		<td class="col_label"><h5>Password:</h5></td>
 <?php
 		echo "<td class='col_value'><input type='password' name='PASS' value='$pass' size='16'></td>";
 ?>
 	    </tr>
-	    <tr class="row_odd" title="IP address & port of the TVheadend server, eg 192.168.0.1:9981">
+	    <tr class="row_alt" title="IP address & port of the TVheadend server, eg 192.168.0.1:9981">
 		<td class="col_label"><h5>IP Address:port:</h5></td>
 <?php
 		echo "<td class='col_value'><input type='text' name='IP' value='$ip' size='24'></td>";
 ?>
 	    </tr>
-	    <tr class='row_even' title="TVheadend profile to use for recordings. This line is blank if there is only one profile">
+	    <tr class='row_alt' title="TVheadend profile to use for recordings. This line is blank if there is only one profile">
 <?php
 	$prof = get_profiles();
 	if (isset($prof)) {
@@ -109,7 +109,7 @@
 	    <tr class='heading'>
 		<td colspan='2'><h2>Preferences</h2></td>
 	    </tr>
-	    <tr class='row_odd' title='Time after which entries appear on tomorrow&apos;s EPG. Useful if you watch TV after midnight'>
+	    <tr class='row_alt' title='Time after which entries appear on tomorrow&apos;s EPG. Useful if you watch TV after midnight'>
 		<td class='col_label'><h5>EPG Day starts at:</h5></td>
 		<td class='col_value'>
 		    <select name='EPGSTART'>";
@@ -121,7 +121,7 @@
 		  echo "</select>
 		</td>
 	    </tr>
-	    <tr class='row_even' title='Screen shown when entering TVHadmin'>
+	    <tr class='row_alt' title='Screen shown when entering TVHadmin'>
 		<td class='col_label'><h5>Home Page:</h5></td>
 		<td class='col_value'>
 		    <select name='HOME'>";
@@ -133,7 +133,7 @@
 		echo "</select>
 		</td>
 	    </tr>
-	    <tr class='row_odd' title='Sort recordings by date or by title (ignoring &apos;New:&apos;)'>
+	    <tr class='row_alt' title='Sort recordings by date or by title (ignoring &apos;New:&apos;)'>
 		<td class='col_label'><h5>Recordings Sort Order:</h5></td>
 		<td class='col_value'>";
 	if (!isset($settings['SORT'])) $settings['SORT'] = 0;
@@ -146,7 +146,7 @@
 		  echo "
 		</td>
 	    </tr>
-	    <tr class='row_even' title='Sort channels by name or Logical Channel Number'>
+	    <tr class='row_alt' title='Sort channels by name or Logical Channel Number'>
 		<td class='col_label'><h5>Channels Sort Order:</h5></td>
 		<td class='col_value'>";
 	if (!isset($settings['CSORT'])) $settings['CSORT'] = 0;
@@ -159,7 +159,7 @@
                   echo "
                 </td>
             </tr>
-	    <tr class='row_odd' title='Which TVH tags are to be used for filtering. Click &apos;Save&apos; after changing this selection to update the other preferences'>
+	    <tr class='row_alt' title='Which TVH tags are to be used for filtering. Click &apos;Save&apos; after changing this selection to update the other preferences'>
 		<td class='col_label'><h5>Media Tags to use for selection:</h5></td>
 		<td class='col_value'>";
 	foreach ($tag as $v=>$t) {
@@ -173,7 +173,7 @@
 	echo "
 		</td>
 	    </tr>
-	    <tr class='row_even' title='Default filter setting for the What&apos;s On Now screen'>
+	    <tr class='row_alt' title='Default filter setting for the What&apos;s On Now screen'>
 		<td class='col_label'><h5>Show in What's On Now:</h5></td>
 		<td class='col_value'>";
 	foreach ($tag as $v=>$t) {
@@ -190,7 +190,7 @@
 	echo "
 		</td>
 	    </tr>
-            <tr class='row_odd' title='Default filter setting for the Timeline screen'>
+            <tr class='row_alt' title='Default filter setting for the Timeline screen'>
                 <td class='col_label'><h5>Show in Timeline:</h5></td>
                 <td class='col_value'>";
         foreach ($tag as $v=>$t) {
@@ -207,7 +207,7 @@
         echo "
                 </td>
             </tr>
-	    <tr class='row_even' title='Default filter setting for Recordings'>
+	    <tr class='row_alt' title='Default filter setting for Recordings'>
 		<td class='col_label'><h5>Show in Recordings:</h5></td>
 		<td class='col_value'>";
 	foreach ($tag as $v=>$t) {
@@ -233,7 +233,7 @@
 	  $c1 = ''; $c2 = 'checked';
 	}
 	echo "
-	    <tr class='row_odd' title='Source of description for the EPG and recordings screens. The correct setting depends on your EPG source'>
+	    <tr class='row_alt' title='Source of description for the EPG and recordings screens. The correct setting depends on your EPG source'>
 		<td class='col_label'><h5>Show in EPG and Recordings:</h5></td>
 		<td class='col_value'>
 		    <label for='summ'>Summary:</label>
@@ -242,7 +242,7 @@
 		    <input type='radio' name='SUMM' value='subtitle' id='subt' $c2>
 		</td>
 	    </tr>
-	    <tr class='row_even' title='Refresh the Whats On Now and Timeline screens every minute'>
+	    <tr class='row_alt' title='Refresh the Whats On Now and Timeline screens every minute'>
 		<td class='col_label'><h5>Refresh Whats On Now and Timeline screens:</h5></td>
 		<td class='col_value'><input type='checkbox' name='REFR'";
 	if (isset($settings['REFR'])) {
@@ -251,7 +251,7 @@
 	echo ">
 		</td>
 	    </tr>
-	    <tr class='row_odd' title='Log in to TVheadend when viewing recordings or live TV through TVHadmin. Not needed if you have an anonymous (&apos;*&apos;) user'>
+	    <tr class='row_alt' title='Log in to TVheadend when viewing recordings or live TV through TVHadmin. Not needed if you have an anonymous (&apos;*&apos;) user'>
 		<td class='col_label'><h5>Send user/pass when Viewing:</h5></td>
 		<td class='col_value'><input type='checkbox' name='NOANON'";
         if (isset($settings['NOANON'])) {
@@ -260,7 +260,7 @@
         echo ">
 		</td>
 	    </tr>
-           <tr class='row_even' title='Show icon instead of channel name. Needs picons installed and imagecache enabled on TVHeadend'>
+           <tr class='row_alt' title='Show icon instead of channel name. Needs picons installed and imagecache enabled on TVHeadend'>
                 <td class='col_label'><h5>Show channel icons in What's On Now & Recordings:</h5></td>
                 <td class='col_value'><input type='checkbox' name='ICONS'";
         if (isset($settings['ICONS'])) {
@@ -269,7 +269,7 @@
         echo ">
                 </td>
             </tr>
-	    <tr class='row_odd' title='Show timer conflicts and alternative showings. Only works for single tuners on networks using CRIDs (series link)'>
+	    <tr class='row_alt' title='Show timer conflicts and alternative showings. Only works for single tuners on networks using CRIDs (series link)'>
 		<td class='col_label'><h5>Detect timer clashes (single tuner only):</h5></td>
 		<td class='col_value'><input type='checkbox' name='CLASHDET'";
 	if (isset($settings['CLASHDET'])) {
@@ -278,7 +278,7 @@
 	echo ">
 	      </td>
 	    </tr>
-	    <tr class='row_even' title='Time period for the Timeline screen'>
+	    <tr class='row_alt' title='Time period for the Timeline screen'>
 		<td class='col_label'><h5>Timeline length:</h5></td>
 		<td class='col_value'>
 		    <select name='TIMESPAN'>";
@@ -296,7 +296,7 @@
 	    <tr class='heading'>
 		<td colspan='3'><h2>Favourite Channels</h2></td>
 	    </tr>
-	    <tr class='row_odd'>
+	    <tr class='row_alt'>
 		<td class='col_channels'>
 		    <select name='all_channels' size='8' multiple='multiple' class='channels'>";
 	    $chans = get_channels();

fav.php
@@ -80,17 +80,10 @@
 	  echo "</td></tr>";
 	  $progs = get_epg($c, $when, $next);
 
-	  $i = 0;
 	  foreach($progs as $p) {
 	    $start = date('H:i', $p["start"]);
 	    $end = date('H:i', $p["stop"]);
-	    if ($i % 2) {
-	      echo "<tr class='row_odd' id='$id'>";
-	    }
-	    else {
-	      echo "<tr class='row_even' id='$id'>";
-	    }
-	    print("<td class='col_duration'>$start - $end</td>");
+	    echo "<tr class='row_alt' id='$id'><td class='col_duration'>$start - $end</td>";
 	    printf("<td class='col_title'><div class='epg_title'>%s</div><div class='epg_subtitle'>%s</div></td>", $p["title"],$p[$settings['SUMM']]);
 	    if (!isset($p['dvrState']) || ($p['dvrState'] != 'scheduled' && $p['dvrState'] != 'recording')) {
 	      $evt = $p["eventId"];
@@ -105,7 +98,6 @@
 	    else {
 	      echo "<td></td></tr>";
 	    }
-	    $i++;
 	    $id++;
 	  }
 	  echo "</table>";

links.php
@@ -47,14 +47,7 @@
 		else $rcnt[$autorec] = 1;
 	    }
 	}
-	$i = 0;
 	foreach($links as $l) {
-	    if ($i % 2) {
-		echo "<tr class='row_odd'>";
-	    }
-	    else {
-		echo "<tr class='row_even'>";
-	    }
 	    $channelname = $cname[$l["channel"]];
 	    if (isset($rcnt[$l['uuid']])) $recs = $rcnt[$l['uuid']];
 	    else $recs = 0;
@@ -63,6 +56,7 @@
 	    $title = stripslashes($l['title']);
 	    $crid = substr(strstr($l['serieslink'], '//'), 2);
 	    echo "
+      <tr class='row_alt'>
 	<td class='col_value'>$timers</td>
 	<td class='col_value'>$recs</td>
 	<td class='col_channel'>$channelname</td>
@@ -70,7 +64,6 @@
 	<td class='wideonly col_channel'>$crid</td>
 	<td class='col_delete'><a href='links.php?uuid={$l['uuid']}'><img src='images\delete.png'></a></td>
       </tr>\n";
-	    $i++;
 	}
  ?>
      </table>

now.php
@@ -82,7 +82,6 @@
      <tr class='newday'>
       <td colspan='5'><span class='date_long'>$wday</span></td>
      </tr>";
-	$i = 0;
 	foreach($chans as $c) {
 		if (!isset($media["All"])) {
 			foreach($c["tags"] as $t) {
@@ -91,8 +90,6 @@
 			continue;
 		}
 good:
-		if ($i % 2) echo "<tr class=\"row_odd\">";
-		else echo "<tr class=\"row_even\">";
 		$e = get_epg_now($c["uuid"]);
 		$p = &$e[0];
 		if ($p) {
@@ -106,6 +103,7 @@ good:
 			if (isset($p[$settings['SUMM']])) $summ = $p[$settings['SUMM']];
 			else $summ = '';
 			echo "
+    <tr class='row_alt'>
       <td class='col_duration'>$start - $end
        <table class='percent' title='$don min&nbsp;/&nbsp;$dur min'>
 	<tr>
@@ -150,7 +148,6 @@ good:
 	<a href='$view_url/play/stream/channel/{$c['uuid']}?title={$c['name']}'><img src='images\play.png' title='Play'></a>
       </td>
      </tr>";
-		$i++;
 	}
 	echo "</table></div>\n";
  ?>

recordings.php
@@ -96,7 +96,6 @@
 	    </tr>
   ";
         $recordings = get_recordings($sort);
-	$i = 0;
 	foreach($recordings as $t) {
 		if ($t["sched_status"] == "scheduled") continue;
 		if (!isset($media["All"])) {
@@ -117,12 +116,7 @@ good:
 		if ($t['sched_status'] == 'completed') {
 			$ok = 1;
 			if ($t['status'] == 'Completed OK') {
-				if ($i % 2) {
-					echo "<tr class='row_odd'>";
-				}
-				else {
-					echo "<tr class='row_even'>";
-				}
+				echo "<tr class='row_alt'>";
 			}
 			else {
 				echo "<tr class='row_error'>";
@@ -164,7 +158,6 @@ good:
 	  <td class='col_stream'><a href='$view_url/play/dvrfile/{$t['uuid']}?title={$title}'><img src='images\play.png' title='Play'></a></td>";
 		else echo "<td></td>";
 		echo "</tr>";
-		$i++;
 	}
 ?>
        </table>

search.php
@@ -41,7 +41,6 @@
 	  </tr>
 	</table>
 	<table class='list'>";
-		$i = 0;
 		$last_prog_date = " ";
 		$results = search_epg("", $find);
 		foreach ($results as $r) {
@@ -51,13 +50,8 @@
 				echo "<tr class='newday'><td colspan='5'><span class='date_long'>$d</span></td></tr>";	
 				$last_prog_date = $d;
 			}
-			if ($i % 2) {
-				echo "<tr class='row_odd'>";
-			}
-			else {
-				echo "<tr class='row_even'>";
-			}
 			echo "
+	<tr class='row_alt'>
 	  <td class='col_duration'>
 	    <span class='time_duration'><span class='time_start'>$t</span></span></td>
 	  <td class='col_channel'>
@@ -77,7 +71,6 @@
 			else {
 				echo "<td></td></tr>";
 			}
-			$i++;
 		}
 		echo "</table>";
 	}

status.php
@@ -97,7 +97,7 @@ window.addEventListener('load',function(event) {
 	  <tr class='heading'>
 	    <td class='col_name' colspan=2><h2>System Information</h2></td>
 	  </tr>
-	  <tr class='row_even'>
+	  <tr class='row_alt'>
 	    <td class='col_channel'>Software Version</td>
 	    <td class='col_name'>{$info['sw_version']}</td>
 	  </tr>
@@ -114,15 +114,15 @@ window.addEventListener('load',function(event) {
     }
     else $recstatus = "";
     echo "
-	  <tr class='row_odd'>
+	  <tr class='row_alt'>
 	    <td class='col_channel'>System Load</td>
 	    <td class='col_name'>$load</td>
 	  </tr>
-	  <tr class='row_even'>
+	  <tr class='row_alt'>
 	    <td class='col_channel'>Recording</td>
 	    <td class='col_name'>$recstatus</td>
 	  </tr>
-	  <tr class='row_odd'>
+	  <tr class='row_alt'>
 	    <td class='col_channel'>Subscriptions</td>
 	    <td class='col_name'>{$currentload->subscriptions}</td>
 	  </tr>
@@ -133,7 +133,6 @@ window.addEventListener('load',function(event) {
   ";
 	$stats = get_input_status();
 	foreach($stats as $s) {
-	  $i = 0;
 	  echo "
       <table class='list'>
 	<tr class='heading'>
@@ -166,17 +165,11 @@ window.addEventListener('load',function(event) {
 	    }    
 	    foreach($s as $key => $val) {
 		if (isset($ignore[$key])) continue;
-		if ($i % 2) {
-		    echo "<tr class='row_odd'>";
-		}
-		else {
-		    echo "<tr class='row_even'>";
-		}
-	    echo "
+		echo "
+      <tr class='row_alt'>
 	<td class='col_channel'>{$params[$key]}</td>
 	<td class='col_name'>$val</td>
       </tr>\n";
-	    $i++;
 	    }
 	    echo "</table>";
 	}

commit ea5ee9a9eef451a1c3557164eaafd4ae2a7845c2

Fix quoting error introduced by 970d2dc.

timers.php
@@ -47,7 +47,7 @@
 	    $stop = strftime("%H:%M", $t["stop_real"]);
 	    $date = strftime("%a %e/%m", $t["start_real"]);
 	    $subtitle = $t["disp_extratext"];
-	    echo "<tr class='row_alt' title='$subtitle'>";
+	    echo "<tr class='row_alt' title=\"$subtitle\">";
 	    if ($t["start_real"] < $now) {
 		echo "<td class='col_info'><img src='images/rec.png'></td>";
 	    }