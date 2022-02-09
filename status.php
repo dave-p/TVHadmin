d9083c3935dd8a2b3dacfcaca7d68d6398947d9e
dave-p <git@pickles.me.uk>
2022-02-09 15:22:00 +0000

    Redesign status screen.

--- status.php
+++ status.php
@@ -6,34 +6,25 @@
 <script>
 window.addEventListener('load',function(event) {
     var tableArr = document.getElementsByClassName('list');
-    var cellWidths = new Array();
+    var cellWidth = 0;
 
     // get widest
     for(i = 0; i < tableArr.length; i++)
     {
-        for(j = 0; j < tableArr[i].rows[1].cells.length; j++)
-        {
-           var cell = tableArr[i].rows[1].cells[j];
-
-           if(!cellWidths[j] || cellWidths[j] < cell.clientWidth)
-                cellWidths[j] = cell.clientWidth;
-        }
+           if(cellWidth < tableArr[i].rows[1].cells[0].clientWidth)
+                cellWidth = tableArr[i].rows[1].cells[0].clientWidth;
     }
 
     // set all columns to the widest width found
     for(i = 0; i < tableArr.length; i++)
     {
-        for(j = 0; j < tableArr[i].rows[1].cells.length; j++)
-        {
-            tableArr[i].rows[1].cells[j].style.width = cellWidths[j]+'px';
-        }
+            tableArr[i].rows[1].cells[0].style.width = cellWidth+'px';
     }
 },false);
 </script>
   ";
 
   $params = array(
-	'uuid'	=> 'uuid',
 	'subs'	=> 'Subscribers',
 	'weight'=> 'Weight',
 	'bps'	=> 'Bandwidth',
@@ -77,25 +68,24 @@ window.addEventListener('load',function(event) {
   $info = get_server_info();
   $currentload = simplexml_load_file("$urlp/status.xml");
   echo "
-    <div id='layout'>
-      <div id='banner'>
-	<table>
-	  <tr>
-	    <td class='col_title'><div id='mobmenu'>&#9776;</div> <h1>System Status</h1></td>
-	  </tr>
-	</table>
-      </div>
-      <div id='wrapper'>
-	<div id='content'>
-	<table class='list'>
-	  <tr class='heading'>
-	    <td class='col_name' colspan=2><h2>System Information</h2></td>
-	  </tr>
-	  <tr class='row_alt'>
-	    <td class='col_channel'>Software Version</td>
-	    <td class='col_name'>{$info['sw_version']}</td>
-	  </tr>
-  ";
+<div id='layout'>
+  <div id='banner'>
+    <table>
+      <tr>
+        <td class='col_title'><div id='mobmenu'>&#9776;</div> <h1>System Status</h1></td>
+      </tr>
+    </table>
+  </div>
+  <div id='wrapper'>
+    <div id='content'>
+      <table class='list'>
+        <tr class='heading'>
+          <td class='col_name' colspan=2><h2>System Information</h2></td>
+        </tr>
+        <tr class='row_alt'>
+          <td class='col_channel'>Software Version</td>
+          <td class='col_name'>{$info['sw_version']}</td>
+        </tr>";
   if ($currentload !== FALSE) {
     $load = preg_replace('/(\d{2})\d*/', '$1', $currentload->systemload);
     $load = preg_replace('/,/', ', ', $load);
@@ -108,69 +98,80 @@ window.addEventListener('load',function(event) {
     }
     else $recstatus = "";
     echo "
-	  <tr class='row_alt'>
-	    <td class='col_channel'>System Load</td>
-	    <td class='col_name'>$load</td>
-	  </tr>
-	  <tr class='row_alt'>
-	    <td class='col_channel'>Recording</td>
-	    <td class='col_name'>$recstatus</td>
-	  </tr>
-	  <tr class='row_alt'>
-	    <td class='col_channel'>Subscriptions</td>
-	    <td class='col_name'>{$currentload->subscriptions}</td>
-	  </tr>
-    ";
+        <tr class='row_alt'>
+          <td class='col_channel'>System Load</td>
+          <td class='col_name'>$load</td>
+        </tr>
+        <tr class='row_alt'>
+          <td class='col_channel'>Recording</td>
+          <td class='col_name'>$recstatus</td>
+        </tr>
+        <tr class='row_alt'>
+          <td class='col_channel'>Subscriptions</td>
+          <td class='col_name'>{$currentload->subscriptions}</td>
+        </tr>";
   }
   echo "
-	</table>
-  ";
-	$stats = get_input_status();
-	foreach($stats as $s) {
-	  echo "
+      </table>
       <table class='list'>
-	<tr class='heading'>
-	  <td class='col_name'>
-	    <h2>{$s['input']}</h2>
-	  </td>
-	  <td class='col_name'>
-	    <form name='clear' method='POST' action='status.php'>
-	      <input type='submit' name='clearcounts' value='Clear Counters'>
-	      <input type='hidden' name='uuid' value='{$s["uuid"]}'>
-	    </form>
-	  </td>
-	</tr>";
-	    switch($s['signal_scale']) {
-		case 1:
-		  $s['signal'] = round($s['signal'] * 100 / 65535) . ' %';
-		  break;
-		case 2:
-		  $s['signal'] = round($s['signal'] / 1000, 1) . ' dBm';
-		  break;
-		default:
-		  $s['signal'] = 0;
-	    }
-	    switch($s['snr_scale']) {
-		case 1:
-		  $s['snr'] = round($s['snr'] * 100 / 65535) . ' %';
-                  break;
-		case 2:
-		  $s['snr'] = round($s['snr'] / 1000, 1) . ' dB';
-		  break;
-		default:
-		  $s['snr'] = 0;
-	    }
-	    foreach($s as $key => $val) {
-		if (!isset($params[$key])) continue;
-		echo "
-      <tr class='row_alt'>
-	<td class='col_channel'>{$params[$key]}</td>
-	<td class='col_name'>$val</td>
-      </tr>\n";
-	    }
-	    echo "</table>";
+        <tr class='heading'>
+          <td class='col_name'><h2>Tuners</h2></td>";
+  $stats = get_input_status();
+  foreach($stats as &$s) {
+    echo "
+          <td class='col_name'>
+            <h2>{$s['input']}</h2>
+          </td>";
+    switch($s['signal_scale']) {
+      case 1:
+        $s['signal'] = round($s['signal'] * 100 / 65535) . ' %';
+        break;
+      case 2:
+        $s['signal'] = round($s['signal'] / 1000, 1) . ' dBm';
+        break;
+      default:
+        $s['signal'] = 0;
+    }
+    switch($s['snr_scale']) {
+      case 1:
+        $s['snr'] = round($s['snr'] * 100 / 65535) . ' %';
+        break;
+      case 2:
+        $s['snr'] = round($s['snr'] / 1000, 1) . ' dB';
+        break;
+      default:
+        $s['snr'] = 0;
+    }
+  }
+  unset($s);
+  echo "
+        </tr>
+        <tr>
+          <td></td>";
+  foreach($stats as $s) {
+  echo "
+          <td class='col_name'>
+            <form name='clear' method='POST' action='status.php'>
+              <input type='submit' name='clearcounts' value='Clear Counters'>
+              <input type='hidden' name='uuid' value='{$s["uuid"]}'>
+            </form>
+          </td>";
 	}
- ?>
+  echo "
+        </tr>";
+  foreach($params as $k => $v) {
+    echo "
+        <tr class='row_alt'>
+          <td class='col_channel'>$v</td>";
+    foreach($stats as $s) {
+      if(isset($s[$k])) echo "<td class='col_name'>{$s[$k]}</td>";
+      else echo "<td class='col_name'></td>";
+    }
+    echo "
+        </tr>";
+  }
+?>
+      </table>
      </div>
     </div>
    </div>
