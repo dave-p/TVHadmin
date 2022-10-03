aa9436a5e14b1f5f03ed88786529a601984112d3
dave-p <git@pickles.me.uk>
2022-10-03 09:43:47 +0100

    Add webmanifest.

--- head.php
+++ head.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="application-name" content="TVHadmin">
 <title>TVHadmin - <?php echo $page_title; ?></title>
+<link rel="manifest" href="manifest.webmanifest" crossorigin="use-credentials">
 <?php
   include_once './include.php';
   if (isset($settings["THEME"]) && ($settings["THEME"] == 1)) {
